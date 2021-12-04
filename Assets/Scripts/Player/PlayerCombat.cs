148a5920ab55390ee4ed74942a25d92a9da778d1
Radu Matusa <matusaradu@gmail.com>
2021-12-04 11:56:20 +0100

    Empowering one hits

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -119,24 +119,9 @@ public class PlayerCombat : MonoBehaviour
             return;
         }
 
-        CheckDirectionChange();
+        //CheckDirectionChange();
         
-        if(onWall)
-        {
-            StartCombo(onWall);
-        }
-        else if(shouldFirstAttack && !onWall)
-        {
-            StartCombo();
-        }
-        else if(!shouldFirstAttack && canContinueCombo)
-        {
-            ContinueCombo();
-        }
-        else if(!canContinueCombo)
-        {
-            StopCombo();
-        }
+        StartCombo();
     }
 
     /*Checks if the direction of the attack changed, if yes it resets the visual cue, stops the attack coroutine 
